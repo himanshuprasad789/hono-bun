54650223ef83bb0bf8e1d8e0a933be8ff5c56cb7
himanshu prasad <prasadhimanshu789@gmail.com>
2023-04-03 21:09:15 +0530

    init

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
 import { Hono } from "hono";
-import { serveStatic } from 'hono/serve-static.bun';
-
+import { bearerAuth } from 'hono/bearer-auth'
+import { html } from 'hono/html'
 const port = parseInt(process.env.PORT) || 3000;
 
 const app = new Hono();
+const token = "secret"
 
-app.use('/favicon.ico', serveStatic({ path: './public/favicon.ico' }));
+app.use('/api/*', bearerAuth({ token }))
+app.get("/",(c)=>{
+  return c.text("hello bun/hono user")
+})
+app.get('/api/random', (c) => {
+  const random = Math.floor(Math.random() * 10) * 100;
+  return c.json({ random: random }, 200)
+})
+app.get('/html', (c) => {
+  return c.html(
+    html`<head><title>hello</title></head><h1>HELLO WORLD</h1>`
+  )
+})
 
-app.get("/", (c) => {
-  return c.json({ message: "Hello World!" });
-});
-
-console.log(`Running at http://localhost:${port}`);
 
 export default {
   port,
